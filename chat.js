63cbd75fe02dc6719693cb3d224bee3ec398ef67
vitinhou9 <135711616+vitinhou9@users.noreply.github.com>
2024-01-09 20:22:26 -0300

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,23 @@
+let listaMensagens = [];
+function adicionarMensagem (listaMensagens)
+{
+  let apelido : "", 
+  let mensagem: "",
+}
+
+}
+let input = document this.commitMessageClickHandler ('item');
+
+
+// --------------------------------
+// Não remover estas linhas
+// --------------------------------
+module.exports =
+{
+  listaMensagens,
+  adicionarMensagem,
+  formatarMensagens,
+  atualizarHTML,
+  commitMessageClickHandler
+};
+// --------------------------------
