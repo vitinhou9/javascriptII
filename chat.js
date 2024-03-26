69425d84da2494ac71db6238a9be881a60b2aeb2
vitinhou9 <135711616+vitinhou9@users.noreply.github.com>
2024-03-26 08:47:44 -0300

    teste 2

--- chat.js
+++ chat.js
@@ -1,39 +1,64 @@
 
-let listaMensagens = [{}];
+const listaMensagens = [];
 
-function adicionarMensagem(apelido, mensagem) {
-    listaMensagens.push({
-        "apelido": apelido,
-        "mensagem": mensagem
-    });
+const messageCommit = document.querySelector('#message-commit');
+const messageInput = document.querySelector('#message-input');
+const divpai = document.querySelector('chat-container');
+
+
+function adicionarMensagem (apelido,mensagem)
+{
+    const estrutura = {
+        apelido : apelido, 
+        mensagem : mensagem };
+
+
+    listaMensagens.push(estrutura);
 }
+function formatarMensagens(){
+    const chatmessagediv = document.createElement('div');
+    chatmessagediv.classList.add('chat-message');
 
-function formatarMensagens() {
-    let mensagensHTML = "";
-    for (let mensagem of listaMensagens) {
-        mensagensHTML += `
-            <div class="chat-message">
-                <span class="chat-message-apelido">${mensagem.apelido}</span>
-                <span class="chat-message-item">${mensagem.mensagem}</span>
-            </div>
-        `;
-    }
-    return mensagensHTML;
+        for(let i =0;i < listaMensagens.length ;i++){
+            
+    //apelido
+            const chatMessageApelidoSpan = document.createElement('span');
+                chatMessageApelidoSpan.classList.add('chat-message-apelido');
+                chatMessageApelidoSpan.innerText =listaMensagens[i].apelido;
+    //mensagem
+            const BrElement = document.createElement('br')
+
+            const Message = document.createElement('span');
+                Message.classList.add('chat-message-item');
+                Message.innerText = ` ${listaMensagens[i].mensagem}`;
+
+
+        chatmessagediv.appendChild(chatMessageApelidoSpan);
+        chatmessagediv.appendChild(Message);
+        chatmessagediv.appendChild(BrElement);
+
+        }
+    return chatmessagediv;
 }
-
 function atualizarHTML() {
-    const chatMessagesDiv = document.getElementById('chat-messages');
-    chatMessagesDiv.innerHTML = formatarMensagens();
+    const chatMessages = document.querySelector('#chat-messages');
+    chatMessages.appendChild(formatarMensagens());
 }
-
-function commitMessageClickHandler() {
-    const messageInput = document.getElementById('message-input');
-    const message = messageInput.value.trim();
-    if (message !== "") {
-        adicionarMensagem("Usuário", message);
-        atualizarHTML();
-        messageInput.value = "";
-    }
+function commitMessageClickHandler(){
+    const apelido = ' Vitor ';
+    const message = messageInput.value.trim(); 
+        if (message ==='')
+        {
+            alert('campo vazio!')
+            messageInput.value = "";
+            messageInput.focus();
+        }
+    
+    adicionarMensagem(apelido,message);
+    atualizarHTML();
+    formatarMensagens();
+    messageInput.value = "";
+    messageInput.focus();
 }
 
 module.exports = { 
